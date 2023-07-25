cee84b0b246271dfd725f1bea16d0ed238dcbd01
nedim-m <nedim.misic1@gmail.com>
2023-07-25 10:23:06 +0200

    Style Homepage
    Added assets and style to homepage

--- ekarte_mobile/lib/main.dart
+++ ekarte_mobile/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:ekarte_mobile/screens/bottom_bar.dart';
+import 'package:ekarte_mobile/utils/app_styles.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,9 +13,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: primary,
       ),
       home: const BottomBar(),
     );
