ceddd43e74762f6b4574e0da8250decbc9a404cf
nedim-m <nedim.misic1@gmail.com>
2023-08-02 20:45:56 +0200

    Update search_screen.dart
    search done

--- ekarte_mobile/lib/screens/search_screen.dart
+++ ekarte_mobile/lib/screens/search_screen.dart
@@ -94,6 +94,7 @@ class SearchScreen extends StatelessWidget {
               bigText: "Upcoming Flights", smallText: "View all"),
           Gap(AppLayout.getHeight(25)),
           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Container(
                 height: AppLayout.getHeight(400),
@@ -123,9 +124,96 @@ class SearchScreen extends StatelessWidget {
                         fit: BoxFit.cover,
                       ),
                     ),
-                  )
+                  ),
+                  Gap(AppLayout.getHeight(12)),
+                  Text(
+                      "20% discount on the early booking of this fligth. Don't miss out this chance",
+                      style: Styles.headLineStyle2),
                 ]),
               ),
+              Column(
+                children: [
+                  Stack(
+                    children: [
+                      Container(
+                        width: size.width * 0.44,
+                        height: AppLayout.getHeight(200),
+                        decoration: BoxDecoration(
+                            color: const Color(0xFF3AB8B8),
+                            borderRadius:
+                                BorderRadius.circular(AppLayout.getHeight(18))),
+                        padding: EdgeInsets.symmetric(
+                            vertical: AppLayout.getHeight(15),
+                            horizontal: AppLayout.getWidth(15)),
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Discount\nfor survey",
+                                style: Styles.headLineStyle2.copyWith(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white),
+                              ),
+                              Gap(AppLayout.getHeight(10)),
+                              Text(
+                                "Take the survey about our services and and get discount",
+                                style: Styles.headLineStyle2.copyWith(
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.white,
+                                    fontSize: 18),
+                              ),
+                            ]),
+                      ),
+                      Positioned(
+                        right: -45,
+                        top: -40,
+                        child: Container(
+                          padding: EdgeInsets.all(AppLayout.getHeight(30)),
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              border: Border.all(
+                                  width: AppLayout.getWidth(18),
+                                  color: const Color(0xFF189999)),
+                              color: Colors.transparent),
+                        ),
+                      )
+                    ],
+                  ),
+                  Gap(AppLayout.getHeight(10)),
+                  Container(
+                    width: size.width * 0.44,
+                    height: AppLayout.getHeight(190),
+                    padding: EdgeInsets.symmetric(
+                        vertical: AppLayout.getHeight(15),
+                        horizontal: AppLayout.getWidth(15)),
+                    decoration: BoxDecoration(
+                        borderRadius:
+                            BorderRadius.circular(AppLayout.getHeight(18)),
+                        color: const Color(0xFFEC6545)),
+                    child: Column(children: [
+                      Text(
+                        "Test test",
+                        style: Styles.headLineStyle2.copyWith(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center,
+                      ),
+                      Gap(AppLayout.getHeight(5)),
+                      RichText(
+                        text: const TextSpan(
+                          children: [
+                            TextSpan(
+                                text: ':P', style: TextStyle(fontSize: 38)),
+                            TextSpan(
+                                text: ':)', style: TextStyle(fontSize: 50)),
+                            TextSpan(
+                                text: '*-)', style: TextStyle(fontSize: 38)),
+                          ],
+                        ),
+                      ),
+                    ]),
+                  ),
+                ],
+              )
             ],
           )
         ],
